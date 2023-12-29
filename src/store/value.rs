86ed0b7443b1436c690b4325057be49bb2e5e2af
maxmielchen <max@mielchen.email>
2023-12-29 14:31:34 +0100

    Add new value types to Value enum

--- src/store/value.rs
+++ src/store/value.rs
@@ -3,4 +3,8 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Value {
     Text(String),
+    Number(i128),
+    Boolean(bool),
+    Array(Vec<Value>),
+    Map(Vec<(String, Value)>),
 }

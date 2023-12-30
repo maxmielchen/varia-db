8d09dd15ab8eb70a911a7f3e65111facbdca098d
maxmielchen <max@mielchen.email>
2023-12-30 01:08:59 +0100

    Refactor store implementation

--- src/store/disk.rs
+++ src/store/disk.rs
@@ -4,11 +4,11 @@ use log::error;
 
 use super::Value;
 
-pub struct Secondary {
+pub struct Disk {
     path: PathBuf,
 }
 
-impl Secondary {
+impl Disk {
     pub fn new(path: &Path) -> Result<Self, Error> {
         if path.exists() {
             if !path.is_dir() {

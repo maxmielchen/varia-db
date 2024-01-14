a404cc3c37cc77d0d02ad793ab47512a6f695f47
maxmielchen <max@mielchen.email>
2024-01-14 19:21:06 +0100

    Refactor key-value storage logic

--- src/store/engine.rs
+++ src/store/engine.rs
@@ -40,8 +40,43 @@ impl Engine {
         info!("PUT {:?} {:?}", key, value);
 
         Self::key_validation(&key)?;
+        
+        if let Some(current_value) = self.primary.get(&key).await {
 
-        unimplemented!()
+            self.secondary.lock().unwrap().put(key.clone(), value.clone())?;
+
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), value.clone());
+            tokio::spawn(async move {
+                primary.insert(key_clone, Some(value_clone)).await;
+            });
+
+            return Ok(current_value);
+
+        } else if let Some(current_value) = self.secondary.lock().unwrap().get(key.clone())? {
+
+            self.secondary.lock().unwrap().put(key.clone(), value.clone())?;
+            
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), value.clone());
+            tokio::spawn(async move {
+                primary.insert(key_clone, Some(value_clone)).await;
+            });
+
+            return Ok(Some(current_value));
+
+        } else {
+            self.secondary.lock().unwrap().put(key.clone(), value.clone())?;
+            
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), value.clone());
+            tokio::spawn(async move {
+                primary.insert(key_clone, Some(value_clone)).await;
+            });
+
+            return Ok(None);
+
+        }
     }
 
     pub async fn get(&self, key: String) -> Result<Option<Value>, Error> {
@@ -49,26 +84,86 @@ impl Engine {
         
         Self::key_validation(&key)?;
 
-        unimplemented!()
+        if let Some(value) = self.primary.get(&key).await {
+
+            return Ok(value);
+
+        } else if let Some(value) = self.secondary.lock().unwrap().get(key.clone())? {
+
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), value.clone());
+            tokio::spawn(async move {
+                primary.insert(key_clone, Some(value_clone)).await;
+            });
+
+            return Ok(Some(value));
+
+        } else {
+
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), None);
+            tokio::spawn(async move {
+                primary.insert(key_clone, value_clone).await;
+            });
+
+            return Ok(None);
+
+        }
     }
 
     pub async fn del(&self, key: String) -> Result<Option<Value>, Error> {
         info!("DEL {:?}", key);
         Self::key_validation(&key)?;
 
-        unimplemented!()
+        
+        if let Some(value) = self.primary.get(&key).await {
+
+            self.secondary.lock().unwrap().del(key.clone())?;
+            
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), None);
+            tokio::spawn(async move {
+                primary.insert(key_clone, value_clone).await;
+            });
+
+            return Ok(value);
+
+        } else if let Some(value) = self.secondary.lock().unwrap().get(key.clone())? {
+
+            self.secondary.lock().unwrap().del(key.clone())?;
+            
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), None);
+            tokio::spawn(async move {
+                primary.insert(key_clone, value_clone).await;
+            });
+
+            return Ok(Some(value));
+
+        } else {
+
+            let primary = self.primary.clone();
+            let (key_clone, value_clone) = (key.clone(), None);
+            tokio::spawn(async move {
+                primary.insert(key_clone, value_clone).await;
+            });
+
+            return Ok(None);
+
+        }
+        
     }
 
     pub async fn list(&self) -> Result<Vec<String>, Error> {
         info!("LIST");
 
-        unimplemented!()
+        return Ok(self.secondary.lock().unwrap().list()?);
     }
 
     pub async fn clear(&self) -> Result<(), Error> {
         info!("CLEAR");
 
-        unimplemented!()
+        return Ok(self.secondary.lock().unwrap().clear()?);
     }
 }
 
